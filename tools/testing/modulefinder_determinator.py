8d74433ff5376f77687b6091b2c6516faf3a4526
Michael Andreas Dagitses <mikeyd@fb.com>
2021-08-27 19:19:28 +0000

    add mechanism to allow for tools imports in our tests
    We of course want to have lower level tests of our codegen code. But
    right now we can't import any of the code to use in a non-end-to-end
    manner.
    
    See #61296 which is trying to get a codegen test in CI for more
    motivation.
    
    ghstack-source-id: 6cdc7636e14f7ace7c5748ddd52e22f8224c6878
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/63946

--- tools/testing/modulefinder_determinator.py
+++ tools/testing/modulefinder_determinator.py
@@ -203,6 +203,7 @@ def get_dep_modules(test: str) -> Set[str]:
             "mpl_toolkits",
             "google",
             "onnx",
+            "utils",  # REPO_ROOT/test/utils
             # Triggers RecursionError
             "mypy",
         ],
