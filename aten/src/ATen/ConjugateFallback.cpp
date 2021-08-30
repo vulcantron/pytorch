05d213703c902c7d740fd75ca1b06fc74c286b69
anjali411 <chourdiaanjali123@gmail.com>
2021-08-30 07:43:53 -0700

    Add fast path for addmm when the inputs are conjugate
    ghstack-source-id: 1101e1e602fe68c94e8d56bfa54e043125ee59ef
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/59380

--- aten/src/ATen/ConjugateFallback.cpp
+++ aten/src/ATen/ConjugateFallback.cpp
@@ -60,6 +60,18 @@ TORCH_LIBRARY_IMPL(aten, Conjugate, m) {
   m.impl("vdot", torch::CppFunction::makeFallthrough());
   m.impl("dot.out", torch::CppFunction::makeFallthrough());
   m.impl("vdot.out", torch::CppFunction::makeFallthrough());
+  m.impl("alias", torch::CppFunction::makeFallthrough());
+  m.impl("mm", torch::CppFunction::makeFallthrough());
+  m.impl("mm.out", torch::CppFunction::makeFallthrough());
+  m.impl("addmm", torch::CppFunction::makeFallthrough());
+  m.impl("addmm_", torch::CppFunction::makeFallthrough());
+  m.impl("addmm.out", torch::CppFunction::makeFallthrough());
+  // TODO: enable these after the cublas bug is fixed: https://github.com/pytorch/pytorch/issues/64103
+  // m.impl("bmm", torch::CppFunction::makeFallthrough());
+  // m.impl("bmm.out", torch::CppFunction::makeFallthrough());
+  // m.impl("baddbmm", torch::CppFunction::makeFallthrough());
+  // m.impl("baddbmm_", torch::CppFunction::makeFallthrough());
+  // m.impl("baddbmm.out", torch::CppFunction::makeFallthrough());
 }
 
 } // namespace at
